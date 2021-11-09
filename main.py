c6aa2bdf415ced01b9ab3321dc126779a46728dd
Eddine Omar <eddine.farid@hotmail.fr>
2021-11-09 09:23:28 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import lxml.etree
+tree = lxml.etree.parse('mayotte')
+
+list_amenity = []
+set_amenity = set()
+
+for tag in tree.findall('.//tag/..')[10:]:
+    for child in tag.xpath("./tag"):
+        if child.get("k") == "amenity":
+            set_amenity.add(child.get("v"))
+
+print(set_amenity)
