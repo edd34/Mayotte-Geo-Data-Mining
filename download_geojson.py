f08b1f61346772a15a0d9661bcd7bc698af0a9f3
Eddine Omar <eddine.farid@hotmail.fr>
2021-11-15 07:59:48 +0300

    add download_geojson.py file

---
+++ download_geojson.py
@@ -0,0 +1,9 @@
+import overpass
+import geojson
+from query import query
+api = overpass.API()
+
+res = api.get(query, build=False)
+
+with open("./mayotte.json",mode="w") as f:
+  geojson.dump(res,f)
