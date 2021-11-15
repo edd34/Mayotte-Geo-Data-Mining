c03087842d47a90f1c559e98e17a88f164850136
Eddine Omar <eddine.farid@hotmail.fr>
2021-11-15 08:00:04 +0300

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,23 @@ import geopandas as gpd
 from query import query
 api = overpass.API()
 
-res = api.get(query, build=False)
+with open("./test.json",mode="r") as f:
+  data_load = geojson.load(f)["elements"]
 
-with open("./test.json",mode="w") as f:
-  geojson.dump(res,f)
+# step 1 : extract raw data from the dowloaded json
+# step 2 : transform data
+# step 2.1 : get centroid of relation
+# step 2.2 : get centroid 
+# step 3 : return data in a format easily readable and usable
+
+list_features = ["amenity", "shop"]
+def my_custom_filter(features, x):
+  check = all(item in features for item in list(x["tags"].keys()))
+  if not check:
+    return False
+  if not x["type"] == "node":
+    return False
+  return True
+
+res = filter(lambda x: my_custom_filter(list_features, x), data_load)
+print(list(res))
